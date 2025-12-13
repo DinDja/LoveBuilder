420675dbfc7fd9732adf82ea316fe23a2a4c091e
Bruno <obitoandradeuthiha@gmail.com>
2025-12-13 13:16:59 -0300

    fix: add netlify.toml and relative base

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Garante que os caminhos dos assets sejam absolutos na raiz
-  base: '/', 
+  base: './', // <--- MUDANÇA AQUI: Ponto antes da barra
   build: {
-    // Força a pasta de saída ser 'dist' (padrão do Vite)
-    // Isso evita confusão se o Netlify estiver esperando 'build'
-    outDir: 'dist', 
+    outDir: 'dist',
+    assetsDir: 'assets',
   }
 })
