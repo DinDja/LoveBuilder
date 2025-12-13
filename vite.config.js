2649a48bc2f831ba42e9e30a9d6ceb5fc72a18aa
Bruno <obitoandradeuthiha@gmail.com>
2025-12-13 13:14:56 -0300

    VITE

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Garante que os caminhos dos assets sejam absolutos na raiz
+  base: '/', 
+  build: {
+    // Força a pasta de saída ser 'dist' (padrão do Vite)
+    // Isso evita confusão se o Netlify estiver esperando 'build'
+    outDir: 'dist', 
+  }
 })
